ad8b841025e4a7900bdd69311cab6abd58e776ea
Jason Manners <jasonmanners@gmail.com>
2012-01-25 10:15:11 -0800

    moved update to collection

--- src/collections/zombie_collection.js
+++ src/collections/zombie_collection.js
@@ -1,3 +1,7 @@
 DOT.ZombieCollection = Backbone.Collection.extend({
-	model : DOT.Zombie
+	model : DOT.Zombie,
+
+	update : function(delta_time) {
+		this.collection.each(function(zombie){zombie.update(delta_time)});
+	}
 });
